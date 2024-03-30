c6ed9b275e57a8ac26a6e1eec6eee637c2097a49
Mabedallah <mohamed.altoom@gmail.com>
2024-03-31 07:45:27 +1100

    add some style and prepare NaveBar plus Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import HeroImage from '../assets/profilepic.png'
+import notebookL from '../assets/notebookL.png'
+import notebookM from '../assets/notebookM.png'
+import mobileR from '../assets/mobileR.png'
+
+import{ TypeAnimation } from 'react-type-animation'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div>
+      <img className='' src={notebookL} alt=''/>
+      <img className='' src={notebookM} alt=''/>
+      <img className='' src={mobileR} alt=''/>
+      <div>
+        <h1>
+          <span>
+            <p>
+              I'am a
+            </p>
+          </span>
+          <br/>
+          <TypeAnimation sequence={[
+            "FrontEnd Web Developer",
+            1000,
+            "Full Stack Web Developer",
+            1000,
+            "Mobile App Developer",
+            100
+          ]} repeat={Infinity}>
+
+          </TypeAnimation>
+        </h1>
+        <p>
+
+        </p>
+      </div>
+      <div>
+      <img className='' src={HeroImage} alt=''/>
+      </div>
+    </div>
   )
 }
 
