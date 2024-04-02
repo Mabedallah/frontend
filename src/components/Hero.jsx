b42ddd2df8298b5996e8c75b984e9368b84c2a75
Mabedallah <mohamed.altoom@gmail.com>
2024-04-02 22:47:07 +1100

    Finish the Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,34 +8,36 @@ import{ TypeAnimation } from 'react-type-animation'
 
 const Hero = () => {
   return (
-    <div>
-      <img className='' src={notebookL} alt=''/>
-      <img className='' src={notebookM} alt=''/>
-      <img className='' src={mobileR} alt=''/>
-      <div>
-        <h1>
-          <span>
-            <p>
+    <div className='relative grid sm:grid-cols-3 gap-8 max-w-[1200px] md:h-[70vh] mx-auto py-8'>
+      <img className='absolute z-[-1] w-[400px] bottom-0 left-[-170px]' src={notebookL} alt=''/>
+      <img className='absolute z-[-1] w-[400px] bottom-0 left-[320px]' src={notebookM} alt=''/>
+      <img className='absolute z-[-1] w-[200px] bottom-[500px] left-[450px]' src={mobileR} alt=''/>
+      <div className='glass px-5 py-3 col-span-2 my-auto'>
+        <h1 className='text-white text-4xl sm:text-5xl lg:text-8xl font-extrabold'>
+            <p className='mb-4 text-gray-400'>
               I'am a
             </p>
-          </span>
-          <br/>
           <TypeAnimation sequence={[
             "FrontEnd Web Developer",
             1000,
             "Full Stack Web Developer",
             1000,
             "Mobile App Developer",
-            100
-          ]} repeat={Infinity}>
+            1000,
+          ]}
+          wrapper='span'
+          speed={40}
+          repeat={Infinity}
+          >
 
           </TypeAnimation>
         </h1>
-        <p>
-
+        <p className=' font-bold my-6 sm:text-lg lg:text-xl max-w-[200px] md:max-w-[600px] ml-4
+        text-gray-400'>
+          My name is Mohamed Abed Allah and I am a 5+ years experienced Full Stack software Developer
         </p>
       </div>
-      <div>
+      <div className='absolute bottom-0 right-0 lg:w-[600px] w-[200px] '>
       <img className='' src={HeroImage} alt=''/>
       </div>
     </div>
